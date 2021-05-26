3e2568083adf349a47abc93cd794adf27c61ed00
Philip Heltweg <pheltweg@gmail.com>
2021-05-26 17:57:28 +0200

    New FormulaInput component

--- src/components.ts
+++ src/components.ts
@@ -5,9 +5,11 @@ import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from '@rhazn/logic-components';
 
+import { defineCustomElements } from '@rhazn/logic-components/loader';
 
-
+defineCustomElements();
 export const CreateSyntax = /*@__PURE__*/createReactComponent<JSX.CreateSyntax, HTMLCreateSyntaxElement>('create-syntax');
+export const FormulaInput = /*@__PURE__*/createReactComponent<JSX.FormulaInput, HTMLFormulaInputElement>('formula-input');
 export const PropositionalWorldComponent = /*@__PURE__*/createReactComponent<JSX.PropositionalWorldComponent, HTMLPropositionalWorldComponentElement>('propositional-world-component');
 export const ShowSignature = /*@__PURE__*/createReactComponent<JSX.ShowSignature, HTMLShowSignatureElement>('show-signature');
 export const WorldPreference = /*@__PURE__*/createReactComponent<JSX.WorldPreference, HTMLWorldPreferenceElement>('world-preference');
