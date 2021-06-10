1754e421bb54db90981d6378855a2a66d4abd0e9
Philip Heltweg <pheltweg@gmail.com>
2021-06-10 10:41:04 +0200

    renamed syntax to signature

--- src/components.ts
+++ src/components.ts
@@ -8,7 +8,7 @@ import type { JSX } from '@rhazn/logic-components';
 import { defineCustomElements } from '@rhazn/logic-components/loader';
 
 defineCustomElements();
-export const CreateSyntax = /*@__PURE__*/createReactComponent<JSX.CreateSyntax, HTMLCreateSyntaxElement>('create-syntax');
+export const CreateSignature = /*@__PURE__*/createReactComponent<JSX.CreateSignature, HTMLCreateSignatureElement>('create-signature');
 export const FormulaInput = /*@__PURE__*/createReactComponent<JSX.FormulaInput, HTMLFormulaInputElement>('formula-input');
 export const PropositionalWorldComponent = /*@__PURE__*/createReactComponent<JSX.PropositionalWorldComponent, HTMLPropositionalWorldComponentElement>('propositional-world-component');
 export const ShowSignature = /*@__PURE__*/createReactComponent<JSX.ShowSignature, HTMLShowSignatureElement>('show-signature');
